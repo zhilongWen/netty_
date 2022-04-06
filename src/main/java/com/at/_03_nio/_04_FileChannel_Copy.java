8b6e6e941153be654f8e44bdf55ee44ea33e3cad
zero <zero@zero.com>
2022-04-06 13:07:29 +0800

    transferFrom channel之间拷贝

---
+++ src/main/java/com/at/_03_nio/_04_FileChannel_Copy.java
@@ -0,0 +1,39 @@
+package com.at._03_nio;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @create 2022-04-06
+ */
+public class _04_FileChannel_Copy {
+
+    public static void main(String[] args) throws Exception{
+
+        FileInputStream fileInputStream = new FileInputStream("files//1.png");
+        FileChannel inputStreamChannel = fileInputStream.getChannel();
+
+
+        FileOutputStream fileOutputStream = new FileOutputStream("files//2.png");
+        FileChannel outputStreamChannel = fileOutputStream.getChannel();
+
+        //使用transferForm完成拷贝
+        outputStreamChannel.transferFrom(inputStreamChannel,0,inputStreamChannel.size());
+
+
+
+
+        inputStreamChannel.close();
+        fileInputStream.close();
+
+        outputStreamChannel.close();
+        fileOutputStream.close();
+
+
+    }
+
+
+
+}
