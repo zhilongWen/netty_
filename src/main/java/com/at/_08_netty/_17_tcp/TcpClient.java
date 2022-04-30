eb0ef1d5a23a8348060645fad933692d87785544
zero <zero@zero.com>
2022-04-30 21:10:38 +0800

    tcp 粘包 拆包

--- src/main/java/com/at/_08_netty/_17_tcp/TcpClient.java
+++ src/main/java/com/at/_08_netty/_17_tcp/TcpClient.java
@@ -4,7 +4,6 @@ import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
